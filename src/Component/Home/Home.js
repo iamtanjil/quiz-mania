e96901a470986535d0ca8fc3200d307fc7e645a0
iamtanjil <tanjil4work@gmail.com>
2022-10-11 15:42:08 +0600

    home header complete

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h4>this is home</h4>
+        <div className='home'>
+            <div className="home-container">
+                <img src="quiz.jpg" alt="" />
+                <div className="container-text">
+                    <h4>Hey Mate Check your Skill</h4>
+                    <p>This is a free website here you can check your skill. in which category you are expert in.This a free application we build it for you. just check it out. currently we are working on it if you have any idea about this application let us know.</p>
+                </div>
+            </div>
         </div>
     );
 };
